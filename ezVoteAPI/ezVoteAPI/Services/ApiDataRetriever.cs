38f8294b6a9c08b2eecf3b57f0cf05fca377dc40
Greg Friedman <8547788+friedmangreg37@users.noreply.github.com>
2018-10-18 16:56:22 -0400

    Added missing ApiDataRetriever.cs

---
+++ ezVoteAPI/ezVoteAPI/Services/ApiDataRetriever.cs
@@ -0,0 +1,36 @@
+﻿using ezVoteAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ezVoteAPI.Services
+{
+    public class ApiDataRetriever
+    {
+        private static HttpClient client = new HttpClient();
+
+        private static async Task<Contest> GetContestsAsync(string path)
+        {
+            Contest contest = null;
+            var response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                contest = await response.Content.ReadAsAsync<Contest>();
+            }
+            return contest;
+        }
+
+        public static async Task RunAsync()
+        {
+            client.BaseAddress = new Uri("https://apis.google.com/js/client.js?onload=load");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+        }
+    }
+}
