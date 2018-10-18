7f284749a5753598473e8b99dcdc67129fdffe7a
Greg Friedman <8547788+friedmangreg37@users.noreply.github.com>
2018-10-18 14:24:59 -0400

    Added ElectionRace and IssueStance classes

--- ezVoteAPI/ezVoteAPI/Models/Candidate.cs
+++ ezVoteAPI/ezVoteAPI/Models/Candidate.cs
@@ -9,6 +9,12 @@ namespace ezVoteAPI.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Party { get; set; }
         public string Bio { get; set; }
+
+        public int RaceId { get; set; }
+        public ElectionRace Race { get; set; }
+
+        public ICollection<IssueStance> Issues { get; set; }
     }
 }
---
+++ ezVoteAPI/ezVoteAPI/Models/IssueStance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ezVoteAPI.Models
+{
+    public class IssueStance
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Stance { get; set; }
+
+        public int CandidateId { get; set; }
+        public Candidate Candidate { get; set; }
+    }
+}
