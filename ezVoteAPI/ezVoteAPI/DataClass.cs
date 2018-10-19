f22129017ffe7f0fe397dd2e87c2d6874ba3683d
Amit Agarwal <amitkagarwal717@gmail.com>
2018-10-19 09:38:10 -0400

    working locallly
    Removed path dependency

---
+++ ezVoteAPI/ezVoteAPI/DataClass.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ezVoteAPI
+{
+    public class DataClass
+    {
+       public   Dictionary<string, List<String>> data { get; set; }
+        public List<string> d { get; set; }
+        public List<string> d1 { get; set; }
+        public List<string> d2 { get; set; }
+
+        public List<string> d3 { get; set; }
+        public List<string> d4 { get; set; }
+
+        public DataClass()
+        {
+            data = new Dictionary<string, List<string>>();
+            d = new List<string>();
+            d1= new List<string>();
+            d2 =new List<string>();
+            this.d.Add("https://www.ennekingforflorida.com");
+            this.d.Add("https://www.ennekingforflorida.com/on-the-issues");
+            this.d.Add("https://www.ennekingforflorida.com/news");
+            this.d.Add("https://www.ennekingforflorida.com/calendar");
+            this.d.Add("https://www.ennekingforflorida.com/volunteer");
+            this.d.Add("https://www.ennekingforflorida.com/donate");
+            data.Add("ennekingforflorida.com.txt",d);
+            
+
+            this.d1.Add("http://twitter.com/intent/tweet?text=Contact+-+https%3A%2F%2Fwww.nelsonforsenate.com%2Fcontact%2F+via+%40NelsonForSenate");
+            this.d1.Add("https://www.nelsonforsenate.com/bills-story/");
+            this.d1.Add("https://www.nelsonforsenate.com/issues/");
+            this.d1.Add("https://www.nelsonforsenate.com/news/");
+            this.d1.Add("https://www.nelsonforsenate.com/action-center/");
+            this.d1.Add("https://www.nelsonforsenate.com//");
+            this.d1.Add("https://www.nelsonforsenate.com/issue/economy/");
+            this.d1.Add("https://www.nelsonforsenate.com/issue/healthcare/");
+            this.d1.Add("https://www.nelsonforsenate.com/issue/environment/");
+            this.d1.Add("https://www.nelsonforsenate.com/issue/education/");
+            this.d1.Add("https://www.nelsonforsenate.com/issue/consumer-protection/");
+            this.d1.Add("https://www.nelsonforsenate.com/issue/seniors/");
+            this.d1.Add("https://www.nelsonforsenate.com/category/videos/");
+            this.d1.Add("https://www.nelsonforsenate.com/category/press-releases/");
+
+            this.d1.Add("https://www.nelsonforsenate.com/category/press-releases/");
+            data.Add("nelsonforsenate.com",d1);
+            
+           
+            this.d2.Add("https://rickscottforflorida.com/meet-rick/");
+            this.d2.Add("https://rickscottforflorida.com/");
+            this.d2.Add("https://rickscottforflorida.com/news/");
+            this.d2.Add("https://rickscottforflorida.com/hurricane-preparedness/");
+
+            this.d2.Add("https://rickscottforflorida.com/education/");
+
+            this.d2.Add("https://rickscottforflorida.com/environment/");
+            this.d2.Add("https://rickscottforflorida.com/latin-america/");
+            this.d2.Add("https://rickscottforflorida.com/healthcare/");
+            this.d2.Add("https://rickscottforflorida.com/immigration/");
+            this.d2.Add("https://rickscottforflorida.com/infrastructure/");
+            this.d2.Add("https://rickscottforflorida.com/israel/");
+            this.d2.Add("https://rickscottforflorida.com/jobs/");
+            this.d2.Add("https://rickscottforflorida.com/make-washington-work/");
+            this.d2.Add("https://rickscottforflorida.com/public-safety/");
+
+            this.d2.Add("https://rickscottforflorida.com/military-and-veterans/");
+
+            this.data.Add("rickscottforflorida.com",d2);
+
+        }
+        
+       
+
+
+    }
+}
--- ezVoteAPI/ezVoteAPI/Services/ContestRepository.cs
+++ ezVoteAPI/ezVoteAPI/Services/ContestRepository.cs
@@ -40,9 +40,6 @@ namespace ezVoteAPI.Services
         public static List<Issue> LoadCandidateInfo(string name, string Url)
         {
             HttpClient http = new HttpClient();
-            var path= Path.Combine(System.IO.Path.GetFullPath(@"..\..\"), @"C:\Users\aagarwal\Documents\GitHub\hack18-ezVoteAPI\ezVoteAPI\ezVoteAPI\Rsources\Urllists");
-            string[] filePaths =
-                Directory.GetFiles(path, "*.txt");
             List<Issue> myList = new List<Issue>();
             Dictionary<string, string> hash = new Dictionary<string, string>();
             var issueMainUrl = "";
@@ -58,121 +55,118 @@ namespace ezVoteAPI.Services
             IssueKeyWords.Add("seniors");
             IssueKeyWords.Add("job");
 
-
-
-            foreach (var urlList in filePaths)
+            var mydata = new DataClass();
+            
+            foreach (var item in mydata.data)
             {
-                var list = urlList.Split('\\');
-                var nam = list.Last().Split(('.'));
-
-                if (!Url.Contains(nam.First()))
+                if (Url.Contains(item.Key))
                 {
-                    continue;
-                }
 
-                var lines = System.IO.File.ReadLines(urlList);
-                foreach (var line in lines)
-                {
-                    foreach (var kyword in IssueKeyWords)
+                    foreach (var line in item.Value)
                     {
-                        if (line.Contains(kyword))
+                        foreach (var kyword in IssueKeyWords)
                         {
-                            if (!hash.ContainsKey(kyword))
-                                hash.Add(kyword, line);
-                        }
+                            if (line.Contains(kyword))
+                            {
+                                if (!hash.ContainsKey(kyword))
+                                    hash.Add(kyword, line);
+                            }
 
-                        if (line.Contains("issue"))
-                        {
-                            issueMainUrl = line;
+                            if (line.Contains("issue"))
+                            {
+                                issueMainUrl = line;
+                            }
                         }
                     }
-                }
 
-                if (hash.Count >= 2)
-                {
-                    foreach (var url in hash)
+                    if (hash.Count >= 2)
                     {
-                        var issueSta = new Issue();
+                        foreach (var url in hash)
+                        {
+                            var issueSta = new Issue();
+                            Issue issues = new Issue();
+                            BrowserSession b = new BrowserSession();
+                            var response = b.Get(url.Value);
+                            var node = response.SelectSingleNode("//div[@class='floating-container']");
+                            if (node != null)
+                                node.Remove();
+                            node = response.SelectSingleNode("//div[@class='row alert-bar__row']");
+                            if (node != null)
+                                node.Remove();
+
+
+
+
+
+                            foreach (HtmlAgilityPack.HtmlNode lin in response.SelectNodes("//p"))
+                            {
+                                int i = 0;
+
+                                string d = Encoding.ASCII.GetString(
+                                    Encoding.Convert(
+                                        Encoding.UTF8,
+                                        Encoding.GetEncoding(
+                                            Encoding.ASCII.EncodingName,
+                                            new EncoderReplacementFallback(string.Empty),
+                                            new DecoderExceptionFallback()
+                                        ),
+                                        Encoding.UTF8.GetBytes(lin.InnerText)
+                                    )
+                                );
+                                //string inputString = "Räksmörgås";
+                                d = lin.InnerText.Replace("\r", string.Empty);
+                                d = d.Replace("\t", string.Empty);
+                                //d = lin.InnerText.Replace("\u", string.Empty);
+                                d = d.Replace("\n", string.Empty);
+                                d = d.Replace("\u0027ve", string.Empty);
+                                d = d.Replace("\\", string.Empty);
+                                d = HttpUtility.HtmlDecode(d);
+
+
+
+
+                                issueSta.Text = issueSta.Text + d;
+                                i++;
+                                issueSta.Name = url.Key;
+                                if (i == 2)
+                                    break;
+
+                            }
+
+                            myList.Add(issueSta);
+                        }
+                    }
+                    else
+                    {
+
                         Issue issues = new Issue();
                         BrowserSession b = new BrowserSession();
-                        var response = b.Get(url.Value);
+                        var response = b.Get(issueMainUrl);
                         var node = response.SelectSingleNode("//div[@class='floating-container']");
-                        if(node != null )
+                        if (node != null)
                             node.Remove();
-                         node = response.SelectSingleNode("//div[@class='row alert-bar__row']");
+                        node = response.SelectSingleNode("//div[@class='row alert-bar__row']");
                         if (node != null)
                             node.Remove();
 
-
-                        
-
-
                         foreach (HtmlAgilityPack.HtmlNode lin in response.SelectNodes("//p"))
                         {
-                            int i = 0;
-                            
-                            string d = Encoding.ASCII.GetString(
-                                Encoding.Convert(
-                                    Encoding.UTF8,
-                                    Encoding.GetEncoding(
-                                        Encoding.ASCII.EncodingName,
-                                        new EncoderReplacementFallback(string.Empty),
-                                        new DecoderExceptionFallback()
-                                    ),
-                                    Encoding.UTF8.GetBytes(lin.InnerText)
-                                )
-                            );
-                            //string inputString = "Räksmörgås";
-                            d = lin.InnerText.Replace("\r", string.Empty);
-                            d = d.Replace("\t", string.Empty);
-                            //d = lin.InnerText.Replace("\u", string.Empty);
-                            d = d.Replace("\n", string.Empty);
-                            d = d.Replace("\u0027ve", string.Empty);
-                            d = d.Replace("\\", string.Empty);
-                            d =HttpUtility.HtmlDecode(d);
-                            
+                            var header = string.Empty;
+                            if (lin.PreviousSibling == null)
+                            {
+                                continue;
+                            }
 
+                            var issueSta = new Issue();
+                            issueSta.Name = lin.PreviousSibling.InnerText.Replace("\r\n\t", string.Empty);
+                            ;
 
-
-                            issueSta.Text = issueSta.Text + d;
-                            i++;
-                            issueSta.Name = url.Key;
-                            if (i == 2)
-                                break;
-
+                            //// just add the url attribute and pass it up from here
+                            Regex.Replace(lin.InnerText, @"\t|\n|\r", "");
+                            issueSta.Text = issueSta.Text + lin.InnerText.Replace("\r\n\\t", string.Empty);
+                            ;
+                            myList.Add(issueSta);
                         }
-
-                        myList.Add(issueSta);
-                    }
-                }
-                else
-                {
-
-                    Issue issues = new Issue();
-                    BrowserSession b = new BrowserSession();
-                    var response = b.Get(issueMainUrl);
-                    var node = response.SelectSingleNode("//div[@class='floating-container']");
-                    if (node != null)
-                        node.Remove();
-                    node = response.SelectSingleNode("//div[@class='row alert-bar__row']");
-                    if (node != null)
-                        node.Remove();
-
-                    foreach (HtmlAgilityPack.HtmlNode lin in response.SelectNodes("//p"))
-                    {
-                        var header = string.Empty;
-                        if (lin.PreviousSibling == null)
-                        {
-                            continue;
-                        }
-
-                        var issueSta = new Issue();
-                        issueSta.Name = lin.PreviousSibling.InnerText.Replace("\r\n\t", string.Empty); ;
-
-                        //// just add the url attribute and pass it up from here
-                       Regex.Replace(lin.InnerText, @"\t|\n|\r", "");
-                        issueSta.Text = issueSta.Text + lin.InnerText.Replace("\r\n\\t", string.Empty);;
-                        myList.Add(issueSta);
                     }
                 }
 
